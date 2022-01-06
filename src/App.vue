73139abe0fc2b47a0839883c7ddc6bbd456a4426
Kozimbek <rustambekovkozimbek@gmail.com>
2022-01-06 11:50:14 +0200

    add button component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-<div class="container">
-  <h1>Hello world</h1>
-</div>
+  <div class="container">
+    <Header  title="Track"/>
+  </div>
 </template>
 
 <script>
+import Header from  './components/Header'
+
 export default {
   name: "App",
+  components:{
+    Header
+  }
 };
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class=""></div>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
 </template>
 
 <script>
 export default {
-
-}
+  name: "Header",
+  props: {
+    title: String,
+  },
+};
 </script>
 
 <style>
-header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
-
 </style>
