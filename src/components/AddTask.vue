041640810bcfc07cd7f1c1a0b9ca2d0d30682098
Kozimbek <rustambekovkozimbek@gmail.com>
2022-01-06 16:08:45 +0200

    add form

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="">
-    <form class="add-form">
+    <form @submit="onSubmit" class="add-form">
       <div class="form-control">
         <label>Task</label>
-        <input type="text" name="text" placeholder="Add task" />
+        <input type="text" v-model="text" name="text" placeholder="Add task" />
       </div>
       <div class="form-control">
         <label>Date & time</label>
-        <input type="text" name="day" placeholder="Add date & time" />
+        <input
+          type="text"
+          v-model="day"
+          name="day"
+          placeholder="Add date & time"
+        />
       </div>
       <div class="form-control form-control-check">
         <label>Mark</label>
-        <input type="checkbox" name="reminder" />
+        <input type="checkbox" v-model="reminder" name="reminder" />
       </div>
       <input type="submit" value="Save Task" class="btn btn-block" />
     </form>
@@ -21,6 +26,38 @@
 <script>
 export default {
   name: "AddTask",
+  data() {
+    return {
+      text: "",
+      day: "",
+      reminder: false,
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if (!this.text) {
+        alert("Please, add task");
+        return
+      }
+
+      const newTask = {
+        id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder
+      }
+
+      this.$emit('add-task', newTask)
+
+      console.log(newTask);
+
+      this.text = ''
+      this.day = ''
+      this.reminder = false
+    },
+  },
 };
 </script>
 
