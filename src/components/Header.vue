5f54dde8856edc9068adf4a5c73ce06a48bc01ba
Kozimbek <rustambekovkozimbek@gmail.com>
2022-01-06 11:44:45 +0200

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+</style>
